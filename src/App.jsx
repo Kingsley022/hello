7135874101f6d2fdbb3a5ed2f9409c77161a1b63
Kingsley022 <kingsleyokereke911@gmail.com>
2024-01-26 13:31:33 +0100

    Added List

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
+import List from "./components/List"
 import ListGroup from "./components/ListGroup"
 import Management from "./components/Management"
 
 function App(){
-    
+
+    const languages = ['Javascript', "Java", "C++", "Python", "PHP"];
+    const footballers = ['Messi', "Marcus", "Jude"]
+
     return(
         <>
             <ListGroup/>
             <Management/>
+            <List title="programming Languages" list={languages}/>
+            <List title="Footballers" list={footballers} />
+
         </>
     )
 };
--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,20 +1,16 @@
-import { useState } from "react";
+import React from 'react'
 import "../ListGroup.css";
 
 const List = ({list, title}) => {
-  const[currentIndex, setCurrentIndex] = useState(-1);
-
   return (
     <>
-        <h1>List of {title}</h1>
-        <ul className='ul'>
-            {list.map((item, index) => (
-                <li key={item} 
-                className={index === currentIndex ? 'active': null}
-                onClick={() => setCurrentIndex(index)}
-                >{item}</li>
-            ))}
-        </ul>
+      <h1>List of {title}</h1>
+
+      <ul className='ul'>
+        {list.map((item, index) =>(
+          <li key={item}>{item}</li>
+        ))}
+      </ul>
     </>
   )
 }
